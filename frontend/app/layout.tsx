f414b1addc12f557bd65dfcc31a27130b5b435b6
SlavkoMuzdeka <muzdekaslavko@gmail.com>
2025-05-08 13:56:04 +0200

    Added frontend

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,41 @@
+import "@/app/globals.css";
+import type React from "react";
+
+import { Inter, Montserrat } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+import { SettingsProvider } from "@/hooks/use-settings";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={`${inter.variable} ${montserrat.variable}`}
+    >
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem={false}
+          disableTransitionOnChange
+        >
+          <SettingsProvider>{children}</SettingsProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
